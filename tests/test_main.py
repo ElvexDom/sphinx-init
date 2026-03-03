6119307761a4835e9f65beb9ff3e48d89d0f16cf
ElvexDom <n.bertrand@elvex-domotique.fr>
2026-03-03 15:34:12 +0100

    pass

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
 from app.main import main
 
+
 def test_main_output(capsys):
     """Vérifie que la fonction main affiche le bon message de bienvenue."""
     main()
-    
+
     # On capture la sortie de la console
     captured = capsys.readouterr()
-    
+
     # On vérifie que le message attendu est bien présent
     assert "Hello from sphinx-markdown!" in captured.out
